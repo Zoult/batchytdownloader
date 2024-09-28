2758e237950255d4da71432d7356747ffd2076ab
Zoult <fm1416@duck.com>
2024-09-28 18:44:24 +0200

    Update batchdownloader.bat

--- batchdownloader.bat
+++ batchdownloader.bat
@@ -2,11 +2,9 @@
 set ytdlp=https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe
 set ffmpeg=https://github.com/ffbinaries/ffbinaries-prebuilt/releases/download/v6.1/ffmpeg-6.1-win-64.zip
 set ffprobe=https://github.com/ffbinaries/ffbinaries-prebuilt/releases/download/v6.1/ffprobe-6.1-win-64.zip
-set pathed=https://cdn.discordapp.com/attachments/923633338378489856/1195024918329438278/pathed.exe
-set GSharpTools=https://cdn.discordapp.com/attachments/923633338378489856/1195024918673362974/GSharpTools.dll
-set log4net=https://cdn.discordapp.com/attachments/923633338378489856/1195024919126360196/log4net.dll
+set pathed=https://github.com/Zoult/pathed/raw/refs/heads/main/pathed.exe
 set installdir=%USERPROFILE%\Documents\PATH_Programs
-set tempdir=%temp%\ytdownloader
+set tempdir=%temp%\batchdownloader
 
 :yt-dlpCheck
 where /q yt-dlp
@@ -23,7 +21,7 @@ goto :screen
 :screen
 cls
 echo *** This screen will never reappear ***
-echo This YouTube downloader needs %program%,
+echo Batch downloader needs %program%,
 echo it will be installed in %installdir%
 echo.
 set /p cf="Change folder? (y/n) > "
@@ -39,24 +37,21 @@ echo Downloading and installing, please wait
 if %program%==ffmpeg goto :ffmpegDl
 
 :yt-dlpDl
-%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "&{[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}; Invoke-WebRequest -Uri '%ytdlp%' -OutFile '%installdir%\yt-dlp.exe'"
+curl -L -o %installdir%\yt-dlp.exe %ytdlp%
 goto :pathedDl
 
 :ffmpegDl
-%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "&{[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}; Invoke-WebRequest -Uri '%ffmpeg%' -OutFile '%tempdir%\ffmpeg.zip'"
-%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "&{[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}; Invoke-WebRequest -Uri '%ffprobe%' -OutFile '%tempdir%\ffprobe.zip'"
+curl -L -o %tempdir%\ffmpeg.zip %ffmpeg%
+curl -L -o %tempdir%\ffprobe.zip %ffprobe%
 tar -xf "%tempdir%\ffmpeg.zip" -C "%installdir%"
 tar -xf "%tempdir%\ffprobe.zip" -C "%installdir%"
 
 :pathedDl
 echo %path% | findstr /c:%installdir% >nul || (
-    %SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "&{[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}; Invoke-WebRequest -Uri '%pathed%' -OutFile '%tempdir%\pathed.exe'"
-    %SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "&{[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}; Invoke-WebRequest -Uri '%GSharpTools%' -OutFile '%tempdir%\GSharpTools.dll'"
-    %SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "&{[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}; Invoke-WebRequest -Uri '%log4net%' -OutFile '%tempdir%\log4net.dll'"
+    curl -L -o %tempdir%\pathed.exe %pathed%
     %tempdir%\pathed.exe /APPEND %installdir% /USER
 )
 if exist %tempdir% rmdir %tempdir% /s /q
-start "" "%~f0"
 exit
 
 :setdefaults
@@ -64,25 +59,31 @@ set track=bestaudio+bestvideo
 set av=x
 set ao=_
 set vo=_
+set it=_
 set format=mp4
 set mp3=_
 set ogg=_
 set aac=_
 set mp4=x
 set mkv=_
+set avi=_
+set dis=_
 set noconv=_
 
 :settings
 cls
-echo Track.....Enter 1 to switch  #  Convert.....Enter 2 to switch
+echo Track.....Enter 1 to switch  #  Format.....Enter 2 to switch
 echo [%av%] Audio + video            #  [%mp4%] MP4      [%mp3%] MP3
 echo [%ao%] Audio only               #  [%mkv%] MKV      [%ogg%] OGG
-echo [%vo%] Video only               #  [%noconv%] No Conv  [%aac%] AAC
+echo [%vo%] Video only               #  [%avi%] AVI      [%aac%] AAC
+echo                              #  [%dis%] Discord  [%noconv%] No Conv
 echo.
-set /p url="Select option / Paste link: "
+set /p "url=Enter option / Paste link: "
+set "url=%url: =%"
+for /f "tokens=1,* delims=&" %%a in ("%url%") do set "url=%%a"
 
-if %url%==1 ( goto :track
-) else if %url%==2 ( goto :format
+if "%url%"=="1" ( goto :track
+) else if "%url%"=="2" ( goto :format
 ) else if "%url:~0,4%"=="http" goto :download
 goto :settings
 
@@ -101,12 +102,30 @@ if %ao%==x (
 )
 if %vo%==x (
     set vo=_
-    set av=x
-    set track=bestaudio+bestvideo
+    set it=x
+    set track=b
     goto :settings
 )
 
 :format
+if %mp4%==x (
+    set mp4=_
+    set mkv=x
+    set format=mkv
+    goto :settings
+)
+if %mkv%==x (
+    set mkv=_
+    set avi=x
+    set format=avi
+    goto :settings
+)
+if %avi%==x (
+    set avi=_
+    set dis=x
+    set format=dis
+    goto :settings
+)
 if %mp3%==x (
     set mp3=_
     set ogg=x
@@ -121,51 +140,36 @@ if %ogg%==x (
 )
 if %aac%==x (
     set aac=_
-    set mp4=x
-    set format=mp4
-    goto :settings
-)
-if %mp4%==x (
-    set mp4=_
-    set mkv=x
-    set format=mkv
-    goto :settings
-)
-if %mkv%==x (
-    set mkv=_
     set noconv=x
     set format=0
     goto :settings
 )
-if %noconv%==x (
-    set noconv=_
+if %dis%==x (
+    set dis=_
     set mp3=x
     set format=mp3
     goto :settings
 )
+if %noconv%==x (
+    set noconv=_
+    set mp4=x
+    set format=mp4
+    goto :settings
+)
 
 :download
-cls
-yt-dlp --get-title %url% > title.tmp
-set /p title=<title.tmp
-del title.tmp
+for /f "delims=" %%a in ('yt-dlp --get-title %url%') do set "title=%%a"
 
-set "allowedChars=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.-_"
-
-:filterTitle
-if "%title%"=="" goto :filtered
-set "char=%title:~0,1%"
-echo %allowedChars% | find "%char%" >nul
-if %errorlevel%==0 set "filteredTitle=%filteredTitle%%char%"
-set "title=%title:~1%"
-goto :filterTitle
-
-:filtered
-if %format% neq 0 set "ytdlpoutput"="-o ytdlpoutput "
+if %format% neq 0 set ytdlpoutput=-o ytdlpoutput
 yt-dlp -f %track% %ytdlpoutput% %url%
-if %format% neq 0 goto :success
-for %%I in (ytdlpoutput) do set "input=%%I"
-ffmpeg -i "%input%" "%filteredTitle%.%format%"
+if %format%==0 goto :success
+for %%I in (ytdlpoutput*) do set "input=%%I"
+
+if %format%==dis (
+    set format=mp4
+    set scale=-vf "scale=min(640\,iw):min(320\,ih),setsar=1"
+)
+ffmpeg -i "%input%" %scale% "%title%.%format%"
 del %input%
 
 :success
